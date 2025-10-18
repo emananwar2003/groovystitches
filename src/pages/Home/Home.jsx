4187f6f40447333ca95c5160a757d29464a9c067
Basem321 <kmr402050@gmail.com>
2025-10-18 12:50:55 +0300

    Added news page and changed logo in About page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,13 +5,20 @@ const Home = () => {
     <div className="text-center p-10">
       <h1 className="text-4xl font-bold mb-4">Welcome to GroovyStitches!</h1>
       <p className="mb-8">This is the main page of your application.</p>
-
-      <Link
-        to="/about"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Go to About Page
-      </Link>
+      <div className="gap-4 flex justify-center">
+        <Link
+          to="/about"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Go to About Page
+        </Link>
+        <Link
+          to="/news"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Go to News Page
+        </Link>
+      </div>
     </div>
   );
 };
