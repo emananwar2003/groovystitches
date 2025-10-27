88919d72fea93f8cd9e5932ced538265eec5920b
youssefkamal8 <youssefkamal122005@gmail.com>
2025-10-27 18:06:15 +0300

    Footer-v01

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,45 +4,49 @@ import CarouselHome from "../../components/homecomponents/CarouselHome";
 import Homecard from "../../components/homecomponents/Homecard";
 import Washing from "../../components/homecomponents/Washing";
 import Banner from "../../components/homecomponents/Banner";
+// import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
 
 const Home = () => {
   const [trending, setTrending] = useState([
-        {
-            id: 1,
-            img: "bublegum.jpg",
-            title: "bublegum bag",
-            details: "bublegum is your favourite bag for bubbly girls",
-            price: "450",
-        },
-        {
-            id: 2,
-            img: " colorful wallet.jpg",
-            title: " colorful wallet",
-            details: "colorful wallets that can fit your cards and money",
-            price: "150 ",
-        },
-        {
-            id: 3,
-            img: "mashroum.jpg",
-            title: "giant mashroum",
-            details: "our giant mashroum for accessory holding",
-            price: "125 ",
-        },
-        {
-            id: 4,
-            img: "sobya.jpg",
-            title: "sobya",
-            details: "sobya is our seasonal bag  made speacialy for ramdan",
-            price: "550 ",
-        },
-    ]);
+    {
+      id: 1,
+      img: "bublegum.jpg",
+      title: "bublegum bag",
+      details: "bublegum is your favourite bag for bubbly girls",
+      price: "450",
+    },
+    {
+      id: 2,
+      img: " colorful wallet.jpg",
+      title: " colorful wallet",
+      details: "colorful wallets that can fit your cards and money",
+      price: "150 ",
+    },
+    {
+      id: 3,
+      img: "mashroum.jpg",
+      title: "giant mashroum",
+      details: "our giant mashroum for accessory holding",
+      price: "125 ",
+    },
+    {
+      id: 4,
+      img: "sobya.jpg",
+      title: "sobya",
+      details: "sobya is our seasonal bag  made speacialy for ramdan",
+      price: "550 ",
+    },
+  ]);
   return (
     <div className="flex  flex-col justify-center items-center bg-yellow-50 gap-20">
+      {/* <Header></Header> */}
       <FakeComp></FakeComp>
-      <Banner/>
+      <Banner />
       <CarouselHome trending={trending} />
       <Homecard />
-      <Washing/>
+      <Washing />
+      <Footer></Footer>
     </div>
   );
 };
